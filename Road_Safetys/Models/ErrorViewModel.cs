a12209848c6cc86c52da472008515ebfe2395e49
Hiren Lakhatariya <200540107061@darshan.ac.in>
2023-02-25 15:45:06 +0530

    Initial commit

---
+++ Road_Safetys/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Road_Safetys.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
